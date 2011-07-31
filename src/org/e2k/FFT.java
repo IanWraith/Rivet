cebdd59aa52678d2c8e7b37d78b88b65042f3629
IanWraith <ianwraith@gmail.com>
2011-07-31 17:24:19 +0100

    Remove code that isn't needed.

--- src/org/e2k/FFT.java
+++ src/org/e2k/FFT.java
@@ -40,7 +40,7 @@ public class FFT {
 
   double[] window;
   
-  public FFT(int n) {
+  public void Setup(int n) {
     this.n = n;
     this.m = (int)(Math.log(n) / Math.log(2));
 
@@ -107,7 +107,7 @@ public class FFT {
   public void fft(double[] x, double[] y)
   {
     int i,j,k,n1,n2,a;
-    double c,s,e,t1,t2;
+    double c,s,t1,t2;
   
   
     // Bit-reverse
@@ -160,68 +160,5 @@ public class FFT {
 
 
 
-  // Test the FFT to make sure it's working
-  public static void main(String[] args) {
-    int N = 8;
-
-    FFT fft = new FFT(N);
-
-    double[] window = fft.getWindow();
-    double[] re = new double[N];
-    double[] im = new double[N];
-
-    // Impulse
-    re[0] = 1; im[0] = 0;
-    for(int i=1; i<N; i++)
-      re[i] = im[i] = 0;
-    beforeAfter(fft, re, im);
-
-    // Nyquist
-    for(int i=0; i<N; i++) {
-      re[i] = Math.pow(-1, i);
-      im[i] = 0;
-    }
-    beforeAfter(fft, re, im);
-
-    // Single sin
-    for(int i=0; i<N; i++) {
-      re[i] = Math.cos(2*Math.PI*i / N);
-      im[i] = 0;
-    }
-    beforeAfter(fft, re, im);
-
-    // Ramp
-    for(int i=0; i<N; i++) {
-      re[i] = i;
-      im[i] = 0;
-    }
-    beforeAfter(fft, re, im);
-
-    long time = System.currentTimeMillis();
-    double iter = 30000;
-    for(int i=0; i<iter; i++)
-      fft.fft(re,im);
-    time = System.currentTimeMillis() - time;
-    System.out.println("Averaged " + (time/iter) + "ms per iteration");
-  }
-
-  protected static void beforeAfter(FFT fft, double[] re, double[] im) {
-    System.out.println("Before: ");
-    printReIm(re, im);
-    fft.fft(re, im);
-    System.out.println("After: ");
-    printReIm(re, im);
-  }
-
-  protected static void printReIm(double[] re, double[] im) {
-    System.out.print("Re: [");
-    for(int i=0; i<re.length; i++)
-      System.out.print(((int)(re[i]*1000)/1000.0) + " ");
-
-    System.out.print("]\nIm: [");
-    for(int i=0; i<im.length; i++)
-      System.out.print(((int)(im[i]*1000)/1000.0) + " ");
-
-    System.out.println("]");
-  }
+  
 }
