5cfe3fc9d48971ef18b57b04223102453d41c8e9
IanWraith <ianwraith@gmail.com>
2011-08-01 11:58:09 +0100

    Add working FFT code.

--- src/org/e2k/MFSK.java
+++ src/org/e2k/MFSK.java
@@ -2,6 +2,9 @@ package org.e2k;
 
 public class MFSK {
 	
+	private FFT fft=new FFT();
+	private int fft_length=-1;
+	
 	// Do a DCT to return the frequency (in Hz) of a section of the circular buffer
 	public int doDCT (CircularDataBuffer circBuf,WaveData waveData,int start,int length,double samplesPerBaud)	{
 		int bin,k,highbin=-1;
@@ -39,5 +42,42 @@ public class MFSK {
 	    if ((freq>(tone-errorAllow))&&(freq<(tone+errorAllow))) return true;
 	     else return false;
 	  }
+	
+	// Find the bin containing the hight value from an array of doubles
+	private int findHighBin(double[]x)	{
+		int a,highBin=-1;
+		double highVal=-1;
+		for (a=0;a<(x.length/2);a++)	{
+			if (x[a]>highVal)	{
+				highVal=x[a];
+				highBin=a;
+			}
+		}
+		return highBin;
+	}
+	
+	
+	// Given the real data in a double array return the largest frequency component
+	private int getFFTFreq (double[]x,double sampleFreq,int correctionFactor)	{
+		int bin=findHighBin(x);
+		double len=x.length;
+		return (int)((sampleFreq/len)*bin)-correctionFactor;
+	}
+	
+	// Run an FFT on a part of the circular buffer and return the main frequency component
+	public int doFFT (CircularDataBuffer circBuf,WaveData waveData,int start,int length)	{
+		// Check if the FFT length has changed
+		// and if it has setup the FFT object again
+		if (fft_length!=length)	{
+			fft.Setup(length);
+			fft_length=length;
+		}
+		// Get the data from the circular buffer
+	    double datar[]=circBuf.extractDataDouble(start,length);
+	    double datai[]=new double[length];
+	    fft.fft(datar,datai);
+		int freq=getFFTFreq (datar,waveData.sampleRate,waveData.correctionFactor);  
+		return freq;
+	}
 
 }
