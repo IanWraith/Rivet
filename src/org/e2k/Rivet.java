c1501633469e1d35b20608f96d006969d4f214b8
IanWraith <ianwraith@gmail.com>
2011-08-15 14:41:30 +0100

    Add the basic core of XPA2 support.

--- src/org/e2k/Rivet.java
+++ src/org/e2k/Rivet.java
@@ -40,6 +40,7 @@ public class Rivet {
 	public final Font boldFont=new Font("SanSerif",Font.BOLD,12);
 	public final Font italicFont=new Font("SanSerif",Font.ITALIC,12);
     public XPA xpaHandler=new XPA(this,10);	
+    public XPA2 xpa2Handler=new XPA2(this,7.5);	
     public InputThread inputThread=new InputThread(this);
     private DataInputStream inPipeData;
 	private PipedInputStream inPipe;
@@ -166,6 +167,8 @@ public class Rivet {
 		String outLines[]=new String[2];
 		// XPA
 		if (system==1)	outLines=xpaHandler.decode(circBuffer,waveData);
+		// XPA2
+		else if (system==2)	outLines=xpa2Handler.decode(circBuffer,waveData);
 		// Return if nothing at all has been returned
 		if (outLines==null) return;
 		// Display the decode objects output
---
+++ src/org/e2k/XPA2.java
@@ -0,0 +1,145 @@
+package org.e2k;
+
+public class XPA2 extends MFSK {
+	
+	private double baudRate=7.5;
+	private int state=0;
+	private double samplesPerSymbol;
+	private Rivet theApp;
+	private long sampleCount=0;
+	private int symbolCounter=0;
+	private String previousCharacter;
+	private int groupCount=0;
+	private StringBuffer lineBuffer=new StringBuffer();
+	
+	private int lastFFT=-1;
+	
+	public XPA2 (Rivet tapp,double baud)	{
+		baudRate=baud;
+		theApp=tapp;
+	}
+	
+	public void setBaudRate(double baudRate) {
+		this.baudRate = baudRate;
+	}
+
+	public double getBaudRate() {
+		return baudRate;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public int getState() {
+		return state;
+	}
+	
+	public String[] decode (CircularDataBuffer circBuf,WaveData waveData)	{
+		String outLines[]=new String[2];
+				
+		// Just starting
+		if (state==0)	{
+			samplesPerSymbol=samplesPerSymbol(baudRate,waveData.sampleRate);
+			state=1;
+			theApp.setStatusLabel("Start Tone Hunt");
+			return null;
+		}
+		// Hunting for a start tone
+		if (state==1)	{
+			outLines[0]=startToneHunt(circBuf,waveData);
+			if (outLines[0]!=null)	{
+				// Have start tone
+				state=2;
+				theApp.setStatusLabel("Sync Hunt");
+				return outLines;
+			}
+		}
+		
+		
+		// Look for a sync low (1025 Hz) followed by a sync high (600 Hz) then another sync high (1120 Hz)
+		if (state==2)	{
+			final int ERRORALLOWANCE=20;
+			int pos=0;
+			int sfft1=doShortFFT (circBuf,waveData,pos);
+			
+			if (lastFFT!=sfft1)	{
+				String l=Integer.toString(sfft1)+","+Long.toString(sampleCount);
+				theApp.debugDump(l);
+			}
+			lastFFT=sfft1;
+			
+			if (toneTest(sfft1,1025,ERRORALLOWANCE)==false)	{
+				sampleCount++;
+				symbolCounter++;
+				return null;
+			}	
+			pos=(int)samplesPerSymbol-SHORT_FFT_SIZE;
+			int sfft2=doShortFFT (circBuf,waveData,pos);
+			if (toneTest(sfft2,1025,ERRORALLOWANCE)==false)	{
+				sampleCount++;
+				symbolCounter++;
+				return null;
+			}
+			pos=(int)samplesPerSymbol;
+			int sfft3=doShortFFT (circBuf,waveData,pos);
+			if (toneTest(sfft3,1060,ERRORALLOWANCE)==false)	{
+				sampleCount++;
+				symbolCounter++;
+				return null;
+			}
+			pos=pos+(int)samplesPerSymbol-SHORT_FFT_SIZE;
+			int sfft4=doShortFFT (circBuf,waveData,pos);
+			if (toneTest(sfft4,1060,ERRORALLOWANCE)==false)	{
+				sampleCount++;
+				symbolCounter++;
+				return null;
+			}
+			
+			state=3;
+			symbolCounter=0;
+			theApp.setStatusLabel("Sync Achieved");
+			outLines[0]=theApp.getTimeStamp()+" Sync Achieved at position "+Long.toString(sampleCount);
+				
+		}
+		
+		
+		// Get valid data
+		if (state==3)	{
+			// Only do this at the start of each symbol
+			if (symbolCounter==(int)samplesPerSymbol)	{
+				symbolCounter=0;				
+				int freq=symbolFreq(circBuf,waveData,0,samplesPerSymbol);
+				//outLines=displayMessage(freq);
+			}
+		}
+		
+		sampleCount++;
+		symbolCounter++;
+		return outLines;
+	}
+	
+	// Hunt for an XPA2 start tone
+	private String startToneHunt (CircularDataBuffer circBuf,WaveData waveData)	{
+		String line;
+		int shortFreq=doShortFFT(circBuf,waveData,0);
+		// Low start tone
+		if (toneTest(shortFreq,965,50)==true)	{
+			waveData.shortCorrectionFactor=shortFreq-965;
+			int longFreq=doFFT(circBuf,waveData,0,LONG_FFT_SIZE);
+			waveData.longCorrectionFactor=longFreq-965;
+			line=theApp.getTimeStamp()+" XPA2 Low Start Tone Found ("+Integer.toString(longFreq)+" Hz)";
+			return line;
+		}
+		// High start tone
+		//else if (toneTest(shortFreq,1280,50)==true)	{
+			//waveData.shortCorrectionFactor=shortFreq-1280;
+			//int longFreq=doFFT(circBuf,waveData,0,LONG_FFT_SIZE);
+			//waveData.longCorrectionFactor=longFreq-1280;
+			//line=theApp.getTimeStamp()+" XPA2 High Start Tone Found ("+Integer.toString(longFreq)+" Hz)";
+			//return line;
+		//}
+		else return null;
+	}
+
+}
