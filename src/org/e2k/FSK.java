4ffe3fc76603554d7e4fedcd2fb286459deec16e
IanW <ianwraith@gmail.com>
2011-11-14 12:31:02 +0000

    Separate out the FSK code from the MFSK code and make the MFSK class inherit from the FSK one.

--- src/org/e2k/FSK.java
+++ src/org/e2k/FSK.java
@@ -1,135 +1,31 @@
 package org.e2k;
 
-import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
-
 public class FSK {
 	
-	public final int FFT_128_SIZE=128;
-	private DoubleFFT_1D fft128=new DoubleFFT_1D(FFT_128_SIZE);
 	private int highSpectrum;
-	
-	private int baudRate;
-	private int shift;
-	private int state=0;
-	private double samplesPerSymbol;
-	private Rivet theApp;
-	public long sampleCount=0;
-	private long symbolCounter=0;
-	private long energyStartPoint;
-	private StringBuffer lineBuffer=new StringBuffer();
-	private CircularDataBuffer energyBuffer=new CircularDataBuffer();
-	private int highTone;
-	private int lowTone;
-	private int centre;
-	
-	private String line="";
-	private int ccount=0;
-	
-	public FSK (Rivet tapp,int baud)	{
-		baudRate=baud;
-		theApp=tapp;
-	}
+	public double totalEnergy;
 	
 	// Return the number of samples per baud
 	public double samplesPerSymbol (double dbaud,double sampleFreq)	{
 			return (sampleFreq/dbaud);
 		}
 	
-	public String[] decode (CircularDataBuffer circBuf,WaveData waveData)	{
-		String outLines[]=new String[2];
-		
-		if (state==0)	{
-			// sampleCount must start negative to account for the buffer gradually filling
-			sampleCount=0-circBuf.retMax();
-			symbolCounter=0;
-			samplesPerSymbol=samplesPerSymbol(baudRate,waveData.getSampleRate());
-			// Clear the energy buffer
-			energyBuffer.setBufferCounter(0);
-			state=1;
-			highTone=-1;
-			lowTone=9999;
-			line="";
-			ccount=0;
-			return null;
-		}
-		
-		if (state==1)	{
-			
-			sampleCount++;
-			if (sampleCount<0) return null;
-			
-			int f=do128FFT(circBuf,waveData,0);
-			energyBuffer.addToCircBuffer(highSpectrum);
-			
-			if (f>highTone) highTone=f;
-			if (f<lowTone) lowTone=f;
-			
-			sampleCount++;
-			symbolCounter++;
-			// Gather a symbols worth of energy values
-			if (energyBuffer.getBufferCounter()<(int)(samplesPerSymbol*1)) return null;
-			int pos=energyBuffer.returnHighestBin();
-			// Calculate what the value of the symbol counter should be
-			symbolCounter=symbolCounter-pos;
-			centre=(highTone+lowTone)/2;
-			shift=highTone-lowTone;
-			state=2;
-		}
-		
-		if (state==2)	{
-			
-			if (symbolCounter>=(long)samplesPerSymbol)	{
-				symbolCounter=0;		
-				int freq=do128FFT(circBuf,waveData,0);
-				boolean bit=freqDecision(freq);
-				if (bit==true) line=line+"1";
-				else line=line+"0";
-				ccount++;
-				
-				if (ccount==60)	{
-					outLines[0]=line;
-					line="";
-					ccount=0;
-				}
-				
-			}
-				
-		}
-		
-		sampleCount++;
-		symbolCounter++;
-		return outLines;
-	}
-	
-	public void setState(int state) {
-		this.state = state;
-	}
-
-	public int getState() {
-		return state;
-	}
-	
-	public int do128FFT (CircularDataBuffer circBuf,WaveData waveData,int start)	{
-		// Get the data from the circular buffer
-	    double datar[]=circBuf.extractDataDouble(start,FFT_128_SIZE);
-		fft128.realForward(datar);
-		double spec[]=getSpectrum(datar);
-		int freq=getFFTFreq (spec,waveData.getSampleRate());  
-		return freq;
-	}
-	
 	// Combine the complex data returned by the JTransform FFT routine to provide
 	// a power spectrum
 	public double[] getSpectrum (double[]data)	{
 			double spectrum[]=new double[data.length/2];
 			double highS=0;
+			// Clear the total energy sum
+			totalEnergy=0.0;
 			int a,count=0;
 			for (a=2;a<data.length;a=a+2)	{
 				spectrum[count]=Math.sqrt(Math.pow(data[a],2.0)+Math.pow(data[a+1],2.0));
 				if (spectrum[count]>highS) highS=spectrum[count];
+				// Add this to the total energy sum
+				totalEnergy=totalEnergy+spectrum[count];
 				count++;
 			}
-			highSpectrum=(int)highS;
+			setHighSpectrum((int)highS);
 			return spectrum;
 		}
 	
@@ -142,7 +38,7 @@ public class FSK {
 		}
 	
 	// Find the bin containing the hight value from an array of doubles
-	private int findHighBin(double[]x)	{
+	public int findHighBin(double[]x)	{
 			int a,highBin=-1;
 			double highestValue=-1;
 			for (a=0;a<x.length;a++)	{
@@ -154,10 +50,31 @@ public class FSK {
 			// Return the highest bin position
 			return highBin+1;
 		}
-	
-	private boolean freqDecision (int freq)	{
-		if (freq>centre) return true;
-		else return false;
+
+	public int getHighSpectrum() {
+		return highSpectrum;
 	}
+
+	public void setHighSpectrum(int highSpectrum) {
+		this.highSpectrum = highSpectrum;
+	}
+	
+	// Return the total energy sum
+	public double getTotalEnergy ()	{
+			return this.totalEnergy;
+		}
+	
+	// A Hamming window
+	public double windowHamming (double in,int i,int m)	{
+			double r=0.54-0.46*Math.cos(2*Math.PI*i/m);
+			return (in*r);
+		}
+		
+	// A Blackman window
+	public double windowBlackman (double in,int i,int m)	{
+			double r=0.42-0.5*Math.cos(2*Math.PI*i/m)+0.08*Math.cos(4*Math.PI*i/m);
+			return (in*r);
+		}
+
 	
 }
--- src/org/e2k/MFSK.java
+++ src/org/e2k/MFSK.java
@@ -15,7 +15,7 @@ package org.e2k;
 
 import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
 
-public class MFSK {
+public class MFSK extends FSK {
 		
 	public final int FFT_8_SIZE=8;
 	public final int FFT_64_SIZE=64;
@@ -32,53 +32,15 @@ public class MFSK {
 	private DoubleFFT_1D fft128=new DoubleFFT_1D(FFT_128_SIZE);
 	private DoubleFFT_1D fft64=new DoubleFFT_1D(FFT_64_SIZE);
 	private double totalEnergy;
-	private int highestFrequency=-1;
-	private int secondHighestBin=-1;
-	private int thirdHighestBin=-1;
 	private double highestValue;
-	
-	// Return the number of samples per baud
-	public double samplesPerSymbol (double dbaud,double sampleFreq)	{
-		return (sampleFreq/dbaud);
-	}
+
 	
 	// Test for a specific tone
 	public boolean toneTest (int freq,int tone,int errorAllow)	{
 	    if ((freq>(tone-errorAllow))&&(freq<(tone+errorAllow))) return true;
 	     else return false;
 	  }
-	
-	// Find the bin containing the hight value from an array of doubles
-	private int findHighBin(double[]x)	{
-		int a,highBin=-1;
-		highestValue=-1;
-		for (a=0;a<x.length;a++)	{
-			if (x[a]>highestValue)	{
-				highestValue=x[a];
-				// Store the second and third highest bins also
-				thirdHighestBin=secondHighestBin;
-				secondHighestBin=highBin+1;
-				highBin=a;
-			}
-		}
-		// Return the highest bin position
-		return highBin+1;
-	}
 		
-	// Given the real data in a double array return the largest frequency component
-	public int getFFTFreq (double[]x,double sampleFreq)	{
-		int bin=findHighBin(x);
-		double len=x.length*2;
-		double ret=((sampleFreq/len)*bin);
-		// If the returned frequency is higher then the highest request frequency use the next one
-		if ((secondHighestBin!=-1)&&(highestFrequency!=-1))	{
-			if ((int)ret>highestFrequency) ret=((sampleFreq/len)*secondHighestBin);
-			// If the second highest bin frqeuency is to high try the third highest
-			if (((int)ret>highestFrequency)&&(thirdHighestBin!=-1)) ret=((sampleFreq/len)*thirdHighestBin);
-		}
-		return (int)ret;
-	}
-	
 	// We have a problem since FFT sizes must be to a power of 2 but samples per symbol can be any value
 	// So instead I am doing a FFT in the middle of the symbol
 	public int symbolFreq (CircularDataBuffer circBuf,WaveData waveData,int start,double samplePerSymbol)	{
@@ -162,32 +124,6 @@ public class MFSK {
 		return freq;
 	}
 	
-	// Combine the complex data returned by the JTransform FFT routine to provide
-	// a power spectrum
-	public double[] getSpectrum (double[]data)	{
-		double spectrum[]=new double[data.length/2];
-		// Clear the total energy sum
-		totalEnergy=0.0;
-		int a,count=0;
-		for (a=2;a<data.length;a=a+2)	{
-			spectrum[count]=Math.sqrt(Math.pow(data[a],2.0)+Math.pow(data[a+1],2.0));
-			// Add this to the total energy sum
-			totalEnergy=totalEnergy+spectrum[count];
-			count++;
-		}
-		return spectrum;
-	}
-	
-	// Return the total energy sum
-	public double getTotalEnergy ()	{
-		return this.totalEnergy;
-	}
-	
-	// Set the highest frequency you want the object to return
-	public void setHighestFrequencyUsed (int highf)	{
-		highestFrequency=highf;
-	}
-	
 	public int doCR36_8000FFT (CircularDataBuffer circBuf,WaveData waveData,int start)	{
 		// Get the data from the circular buffer
 	    double datao[]=circBuf.extractDataDouble(start,FFT_200_SIZE);
@@ -243,16 +179,4 @@ public class MFSK {
 	}
 	
 	
-	// A Hamming window
-	private double windowHamming (double in,int i,int m)	{
-		double r=0.54-0.46*Math.cos(2*Math.PI*i/m);
-		return (in*r);
-	}
-	
-	// A Blackman window
-	private double windowBlackman (double in,int i,int m)	{
-		double r=0.42-0.5*Math.cos(2*Math.PI*i/m)+0.08*Math.cos(4*Math.PI*i/m);
-		return (in*r);
-	}
-
 }
