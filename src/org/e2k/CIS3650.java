9c99345f93aaefc28177cfe4327db1b9e17c4c38
IanW <ianwraith@gmail.com>
2012-02-09 13:22:39 +0000

    Fix a typo bug in the CIS36-50 symbol sync code.

--- src/org/e2k/CIS3650.java
+++ src/org/e2k/CIS3650.java
@@ -104,7 +104,7 @@ public class CIS3650 extends FSK {
 				// Get the early/late gate difference value
 				double gateDif=gateEarlyLate(circBuf,(int)samplesPerSymbol50);	
 				// Adjust the symbol counter as required to obtain symbol sync
-				if ((symbolCounter<-15)||(symbolCounter>15)) symbolCounter=(int)gateDif/3;
+				if ((gateDif<-15)||(gateDif>15)) symbolCounter=(int)gateDif/3;
 				else symbolCounter=0;
 				// Demodulate a single bit
 				boolean bit=getSymbolBit(circBuf,waveData,0);
