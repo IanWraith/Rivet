f0b7f3e01ac872c101c276be13b716be4529e067
IanW <ianwraith@gmail.com>
2012-05-07 19:17:05 +0100

    Display the received bits of FSK200/1000 data.

--- src/org/e2k/FSK2001000.java
+++ src/org/e2k/FSK2001000.java
@@ -77,7 +77,7 @@ public class FSK2001000 extends FSK {
 		
 		// Hunt for the sync sequence
 		if (state==1)	{
-			//if (sampleCount>0) outLines[0]=syncSequenceHunt(circBuf,waveData);
+			if (sampleCount>0) outLines[0]=syncSequenceHunt(circBuf,waveData);
 			if (outLines[0]!=null)	{
 				state=2;
 				energyBuffer.setBufferCounter(0);
@@ -85,10 +85,130 @@ public class FSK2001000 extends FSK {
 			}
 		}
 		
+		// Decode traffic
+		if (state==2)	{
+			// Only do this at the start of each symbol
+			if (symbolCounter>=samplesPerSymbol)	{
+				symbolCounter=0;
+				int ibit=fsk2001000FreqHalf(circBuf,waveData,0);
+				lineBuffer.append(Integer.toString(ibit));
+				if (characterCount==60)	{
+					outLines[0]=lineBuffer.toString();
+					lineBuffer.delete(0,lineBuffer.length());
+					characterCount=0;
+				}
+				else characterCount++;
+			}
+
+		}
 		
 		sampleCount++;
 		symbolCounter++;
 		return outLines;			
+	}
+	
+	// Look for a sequence of 4 alternating tones with 1000 Hz difference
+	private String syncSequenceHunt (CircularDataBuffer circBuf,WaveData waveData)	{
+		int difference;
+		// Get 4 symbols
+		int freq1=fsk2001000Freq(circBuf,waveData,0);
+		int bin1=getFreqBin();
+		// Check this first tone isn't just noise
+		if (getPercentageOfTotal()<5.0) return null;
+		int freq2=fsk2001000Freq(circBuf,waveData,(int)samplesPerSymbol*1);
+		int bin2=getFreqBin();
+		// Check we have a high low
+		if (freq2>freq1) return null;
+		// Check there is around 1000 Hz of difference between the tones
+		difference=freq1-freq2;
+		if ((difference<975)||(difference>1025) ) return null;
+		int freq3=fsk2001000Freq(circBuf,waveData,(int)samplesPerSymbol*2);
+		// Don't waste time carrying on if freq1 isn't the same as freq3
+		if (freq1!=freq3) return null;
+		int freq4=fsk2001000Freq(circBuf,waveData,(int)samplesPerSymbol*3);
+		// Check 2 of the symbol frequencies are different
+		if ((freq1!=freq3)||(freq2!=freq4)) return null;
+		// Check that 2 of the symbol frequencies are the same
+		if ((freq1==freq2)||(freq3==freq4)) return null;
+		// Store the bin numbers
+		if (freq1>freq2)	{
+			highBin=bin1;
+			lowBin=bin2;
+		}
+		else	{
+			highBin=bin2;
+			lowBin=bin1;
+		}
+		String line=theApp.getTimeStamp()+" FSK200/1000 Sync Sequence Found";
+		return line;
+	}
+	
+	// Find the frequency of a FSK200/1000 symbol
+	// Currently the program only supports a sampling rate of 8000 KHz
+	private int fsk2001000Freq (CircularDataBuffer circBuf,WaveData waveData,int pos)	{
+		// 8 KHz sampling
+		if (waveData.getSampleRate()==8000.0)	{
+			int freq=doFSK200500_8000FFT(circBuf,waveData,pos,(int)samplesPerSymbol);
+			return freq;
+		}
+		return -1;
+	}
+	
+	// The "normal" way of determining the frequency of a FSK200/1000 symbol
+	// is to do two FFTs of the first and last halves of the symbol
+	// that allows us to use the data for the early/late gate and to detect a half bit (which is used as a stop bit)
+	private int fsk2001000FreqHalf (CircularDataBuffer circBuf,WaveData waveData,int pos)	{
+		int sp=(int)samplesPerSymbol/2;
+		// First half
+		double ff1[]=do64FFTHalfSymbolBinRequest (circBuf,pos,sp,lowBin,highBin);
+		// Last half
+		double ff2[]=do64FFTHalfSymbolBinRequest (circBuf,(pos+sp),sp,lowBin,highBin);
+		
+		int high1,high2;
+		if (ff1[0]>ff1[1]) high1=0;
+		else high1=1;
+		if (ff2[0]>ff2[1]) high2=0;
+		else high2=1;
+		
+		double early=ff1[1];
+		double late=ff2[1];
+		addToAdjBuffer(early-late);
+		
+		// Both the same
+		if (high1==high2)	{
+			if (high1==0) return 1;
+			else return 0;
+		}
+		else	{
+				// Is this a stop bit
+				if (high2>high1) return 2;
+				else return 3;
+		}
+	}
+	
+	// Add a comparator output to a circular buffer of values
+	private void addToAdjBuffer (double in)	{
+		adjBuffer[adjCounter]=in;
+		adjCounter++;
+		if (adjCounter==adjBuffer.length) adjCounter=0;
+	}
+	
+	// Return the average of the circular buffer
+	private double adjAverage()	{
+		int a;
+		double total=0.0;
+		for (a=0;a<adjBuffer.length;a++)	{
+			total=total+adjBuffer[a];
+		}
+		return (total/adjBuffer.length);
+	}
+	
+	// Get the average value and return an adjustment value
+	private int adjAdjust()	{
+		double av=adjAverage();
+		if (Math.abs(av)<0.75) return 0;
+		else if (av<0.0) return 1;
+		else return -1;
 	}
 
 }
