5f70585f2cb84e15b2c1a311a348701d66730690
IanW <ianwraith@gmail.com>
2013-02-20 10:13:09 +0000

    Allow the user to delete Triggers.

--- src/org/e2k/TriggerModify.java
+++ src/org/e2k/TriggerModify.java
@@ -18,6 +18,7 @@ public class TriggerModify extends JDialog implements ActionListener {
 	private JButton editTriggerButton=new JButton("Edit the selected Trigger");
 	private JButton okButton=new JButton("OK");
 	private JButton cancelButton=new JButton("Cancel");
+	private boolean changedTriggers=false;
 	
 	public TriggerModify (Rivet theApp)	{
 		TtheApp=theApp;
@@ -36,8 +37,9 @@ public class TriggerModify extends JDialog implements ActionListener {
 		// Add a label to describe the Trigger select combo box
 		JLabel labelCombo=new JLabel("Trigger Select : ");
 		this.add(labelCombo);
-		// Create a combo box
-		createTriggerCombo();
+		// Create a combo box and add it to the dialog
+		populateTriggerCombo();
+		this.add(triggerComboList);
 		// Add a gap between the combo box and the next buttons
 		this.add(new JLabel(""));
 		// Function Buttons
@@ -57,14 +59,13 @@ public class TriggerModify extends JDialog implements ActionListener {
 	}
 	
 	// Create a uneditable combo box showing the current triggers
-	private void createTriggerCombo ()	{
+	private void populateTriggerCombo ()	{
 		// Populate a combo box with a trigger list in it
 		if (triggerList!=null)	{
 			int a;
 			for (a=0;a<triggerList.size();a++)	{
 				triggerComboList.addItem(triggerList.get(a).getTriggerDescription());
 			}
-			this.add(triggerComboList);
 		}
 	}
 	
@@ -94,10 +95,45 @@ public class TriggerModify extends JDialog implements ActionListener {
 	private void deleteTrigger ()	{
 		// Find the name of the selected Trigger
 		String selectedTriggerName=(String) triggerComboList.getSelectedItem();
-		
-		
+		// Get the list index of this trigger
+		int index=getTriggerIndex(selectedTriggerName);
+		// Check if anything has been selected
+		if (index==-1)	{
+			JOptionPane.showMessageDialog(null,"Please select the Trigger you wish to delete.","Rivet", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		// Check the user really wants to do this
+		String chkMsg="Are you sure you wish to delete the trigger : "+selectedTriggerName;
+		if (JOptionPane.showConfirmDialog(this,chkMsg,"Rivet",JOptionPane.YES_NO_OPTION)==0)	{
+			// Delete the trigger
+			triggerList.remove(index);
+			// Ensure the object knows that changes have been made
+			changesMade();
+		}
+	}
+
+	// Inform the main program that the triggers have changed
+	public boolean isChangedTriggers() {
+		return changedTriggers;
 	}
 	
+	// Trigger changes have been made
+	private void changesMade()	{
+		// Record this so the main program knows changes have been made
+		this.changedTriggers=true;
+		// Redraw the trigger combo box
+		triggerComboList.removeAllItems();
+		populateTriggerCombo();
+	}
+
+	// Match a trigger name with its list index else return -1 if nothing found
+	private int getTriggerIndex (String tName)	{
+		int a;
+		for (a=0;a<triggerList.size();a++)	{
+			if (triggerList.get(a).getTriggerDescription().equals(tName)) return a;
+		}
+		return -1;
+	}
 	
 
 }
