a04b4cd16286b97d54b87c37cf21f5abb869d3d1
IanW <ianwraith@gmail.com>
2013-04-24 08:20:46 +0100

    Add basic Junit support.

---
+++ src/test/org/e2k/testGW.java
@@ -0,0 +1,14 @@
+package test.org.e2k;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testGW {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
