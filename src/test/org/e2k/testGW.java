4ee6125b1bfb6f897dc6a985cb14002a3b841fbf
IanW <ianwraith@gmail.com>
2013-05-05 16:02:22 +0100

    Rewrite the GW test class so it uses the ship object.

--- src/test/org/e2k/testGW.java
+++ src/test/org/e2k/testGW.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import junit.framework.TestCase;
 import org.e2k.GW;
+import org.e2k.Ship;
 
 public class testGW extends TestCase {
 
@@ -12,96 +13,67 @@ public class testGW extends TestCase {
 	public void testMMSI() {
 		int a;
 		GW gw=new GW(null);
-		List<Integer> testData=new ArrayList<Integer>();
-		
-		// 235000633 (000) CITY OF AMSTERDAM
-		final int loadData1[]={0x84,0x63,0x66,0x85,0x60,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData1[a]);
-		}
-		if (gw.displayGW_MMSI(testData).indexOf("235000633")==-1) fail("Bad decode of MMSI 235000633");
+		List<Ship> ships=new ArrayList<Ship>();
+		Ship ship=new Ship();
 	
-		// 236313000 (000) STEN MOSTER
-		final int loadData2[]={0x84,0x05,0x02,0x6E,0x66,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData2[a]);
-		}
-		if (gw.displayGW_MMSI(testData).indexOf("236313000")==-1) fail("Bad decode of MMSI 236313000");
-		
-		// 636011947 (000) PORT ARTHUR
-		final int loadData3[]={0x85,0x65,0x22,0xF2,0xE1,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData3[a]);
-		}
-		if (gw.displayGW_MMSI(testData).indexOf("636011947")==-1) fail("Bad decode of MMSI 636011947");
-	
-		// 565494000 (000) SIGAS SILVIA
-		final int loadData4[]={0xD3,0x73,0x72,0xE6,0x66,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData4[a]);
-		}
-		if (gw.displayGW_MMSI(testData).indexOf("565494000")==-1) fail("Bad decode of MMSI 565494000");
-		
 		// 304653000 (000) MARCHICORA
-		final int loadData5[]={0xE0,0x57,0x03,0x66,0x66,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData5[a]);
-		}
-		if (gw.displayGW_MMSI(testData).indexOf("304653000")==-1) fail("Bad decode of MMSI 304653000");
+		int loadData01[]={0xE0,0x57,0x03,0x66,0x66,0x66};
+		ship.setGwIdent(loadData01);
+		ship.setMmsi("304653000");
+		ship.setName("MARCHICORA");
+		ships.add(ship);
 		
-		// 258953000 (000) STEN AURORA
-		final int loadData6[]={0xb4,0x2e,0x83,0x66,0x66,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData6[a]);
-		}
-		if (gw.displayGW_MMSI(testData).indexOf("258953000")==-1) fail("Bad decode of MMSI 258953000");
+		// 565494000 (000) SIGAS SILVIA
+		final int loadData02[]={0xD3,0x73,0x72,0xE6,0x66,0x66};
+		ship.setGwIdent(loadData02);
+		ship.setMmsi("565494000");
+		ship.setName("SIGAS SILVIA");
+		ships.add(ship);	
 		
-		// 235069271 (000) GRETA C
-		final int loadData7[]={0x84,0x63,0x2D,0x14,0x62,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData7[a]);
+		// 236313000 STEN MOSTER
+		final int loadData03[]={0x84,0x05,0x02,0x6E,0x66,0x66};
+		ship.setGwIdent(loadData03);
+		ship.setMmsi("236313000");
+		ship.setName("STEN MOSTER");
+		ships.add(ship);
+		
+		// 477824000 DARYA TARA
+		final int loadData04[]={0x97,0x69,0x74,0x66,0x66,0x66};
+		ship.setGwIdent(loadData04);
+		ship.setMmsi("477824000");
+		ship.setName("DARYA TARA");
+		ships.add(ship);
+		
+		// 235069271 GRETA C
+		final int loadData05[]={0x84,0x63,0x2D,0x14,0x62,0x66};
+		ship.setGwIdent(loadData05);
+		ship.setMmsi("235069271");
+		ship.setName("GRETA C");
+		ships.add(ship);
+		
+		
+		// Test each member of the ships list
+		for (a=0;a<ships.size();a++)	{
+			if (gw.displayGW_MMSI(getGWIdentasArrayList(ships.get(a))).indexOf(ships.get(a).getMmsi())==-1)	{
+				String s="Bad decode of MMSI "+ships.get(a).getMmsi()+" ("+ships.get(a).getName()+")";
+				fail(s);
+			}
 		}
-		if (gw.displayGW_MMSI(testData).indexOf("235069271")==-1) fail("Bad decode of MMSI 235069271");
+		
+		
+		
+	}
 	
-		// 477824000 (000) DARYA TARA
-		final int loadData8[]={0x97,0x69,0x74,0x66,0x66,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData8[a]);
-		}
-		if (gw.displayGW_MMSI(testData).indexOf("477824000")==-1) fail("Bad decode of MMSI 477824000");
 	
-		// 636010534 (000) CUMBRIA
-		final int loadData9[]={0x85,0x65,0x62,0x83,0x67,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData9[a]);
+	// Return the GW ident as a Arraylist of ints
+	private List<Integer> getGWIdentasArrayList (Ship ship)	{
+		List <Integer> gwList=new ArrayList<Integer>();
+		int i;
+		int ida[]=ship.getGwIdent();
+		for (i=0;i<6;i++)	{
+			gwList.add(ida[i]);
 		}
-		if (gw.displayGW_MMSI(testData).indexOf("636010534")==-1) fail("Bad decode of MMSI 636010534");
-	
-		// 235011060 (000) PAUL SCHULTE
-		final int loadData10[]={0x84,0x63,0x22,0x56,0xe6,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData10[a]);
-		}
-		if (gw.displayGW_MMSI(testData).indexOf("235011060")==-1) fail("Bad decode of MMSI 235011060");
-		
-		// 232004630 (000) CITY OF AMSTERDAM
-		final int loadData11[]={0x84,0x64,0x76,0x5,0x66,0x66};
-		testData.clear();
-		for (a=0;a<6;a++) {
-			testData.add(loadData11[a]);
-		}
-		if (gw.displayGW_MMSI(testData).indexOf("232004630")==-1) fail("Bad decode of MMSI 232004630");
-		
+		return gwList;	
 	}
 
 }
