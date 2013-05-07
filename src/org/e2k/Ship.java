27e2cc0a9ab90abd0541c9204c05003eb499e28a
IanW <ianwraith@gmail.com>
2013-05-07 15:53:12 +0100

    Add flag , imo and callsign as members of the ship class.

--- src/org/e2k/Ship.java
+++ src/org/e2k/Ship.java
@@ -5,6 +5,9 @@ public class Ship {
 	private String name;
 	private String mmsi;
 	private int[] gwIdent=new int[6];
+	private String flag;
+	private String imo;
+	private String callsign;
 	
 	public String getName() {
 		return name;
@@ -29,5 +32,30 @@ public class Ship {
 	public void setGwIdent(int[] gwIdent) {
 		this.gwIdent = gwIdent;
 	}
+
+	public String getFlag() {
+		return flag;
+	}
+
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+
+	public String getImo() {
+		return imo;
+	}
+
+	public void setImo(String imo) {
+		this.imo = imo;
+	}
+
+	public String getCallsign() {
+		return callsign;
+	}
+
+	public void setCallsign(String callsign) {
+		this.callsign = callsign;
+	}
 	
+
 }
