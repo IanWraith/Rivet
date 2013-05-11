280d802fd7a10b3e89f8e9069f62d4228ec8f3af
IanW <ianwraith@gmail.com>
2013-05-11 15:24:14 +0100

    Add further MMSI test cases.

--- src/test/org/e2k/testGW.java
+++ src/test/org/e2k/testGW.java
@@ -16,17 +16,17 @@ public class testGW extends TestCase {
 		List<Ship> ships=new ArrayList<Ship>();
 		List<String> errorStrings=new ArrayList<String>();
 		
-		// 304653000 (000) MARCHICORA
+		// 304653000 MARCHICORA
 		final int loadData01[]={0xE0,0x57,0x03,0x66,0x66,0x66};
 		ships.add(createShip("304653000",loadData01));
 		
-		// 565494000 (000) SIGAS SILVIA
-		final int loadData02[]={0xD3,0x73,0x72,0xE6,0x66,0x66};
-		ships.add(createShip("565494000",loadData02));
+		// 565494000 SIGAS SILVIA
+		//final int loadData02[]={0xD3,0x73,0x72,0xE6,0x66,0x66};
+		//ships.add(createShip("565494000",loadData02));
 		
 		// 236313000 STEN MOSTER
-		final int loadData03[]={0x84,0x05,0x02,0x6E,0x66,0x66};
-		ships.add(createShip("236313000",loadData03));
+		//final int loadData03[]={0x84,0x05,0x02,0x6E,0x66,0x66};
+		//ships.add(createShip("236313000",loadData03));
 		
 		// 477824000 DARYA TARA
 		final int loadData04[]={0x97,0x69,0x74,0x66,0x66,0x66};
@@ -47,6 +47,26 @@ public class testGW extends TestCase {
 		// 308574000
 		final int loadData08[]={0xE0,0x36,0xF1,0x66,0x66,0x66};
 		ships.add(createShip("308574000",loadData08));
+		
+		// 371560000
+		final int loadData09[]={0x90,0x32,0x65,0x66,0x66,0x66};
+		ships.add(createShip("371560000",loadData09));
+		
+		// 232004630
+		final int loadData10[]={0x84,0x64,0x76,0x05,0x66,0x66};
+		ships.add(createShip("232004630",loadData10));		
+	
+		// 564110000
+		final int loadData11[]={0xD3,0x27,0x62,0x66,0x66,0x66};
+		ships.add(createShip("564110000",loadData11));		
+		
+		// 477076700
+		final int loadData12[]={0x97,0x61,0x51,0x61,0x66,0x66};
+		ships.add(createShip("477076700",loadData12));	
+		
+		// 211233290
+		final int loadData13[]={0xA4,0x42,0x00,0x2C,0x66,0x66};
+		ships.add(createShip("211233290",loadData13));		
 		
 		// Test each member of the ships list
 		for (a=0;a<ships.size();a++)	{
