cb23b4b5c40de46d9db355b41e8da0319cbdc51a
IanW <ianwraith@gmail.com>
2013-06-30 14:18:53 +0100

    Add a class which contains a 2500 Hz low pass filter.

--- src/org/e2k/PSK2400.java
+++ src/org/e2k/PSK2400.java
@@ -1,9 +1,6 @@
 package org.e2k;
 
 import java.awt.Color;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JOptionPane;
 
 public class PSK2400 extends PSK {
@@ -13,14 +10,8 @@ public class PSK2400 extends PSK {
 	private long sampleCount=0;
 	private long symbolCounter=0;
 	
-	private final int NZEROS_LOW=10;
-	private final double GAIN_LOW=4.194017117e+05;
-	private double xvLow[]=new double[NZEROS_LOW+1];
-	private double yvLow[]=new double[NZEROS_LOW+1];
-	
-	private boolean recoverCarrier=false;
-	
-	
+	private lowPass2500Filter interpolateFilter=new lowPass2500Filter();
+	NCO costasNCO=new NCO(1800.0,0.25,24000);
 	
 	public PSK2400 (Rivet tapp)	{
 		theApp=tapp;
@@ -73,44 +64,46 @@ public class PSK2400 extends PSK {
 			sampleCount++;
 			if (sampleCount<0) return;
 			// For every sample that arrives pad it with two zeros to raise the effective sample rate to 24000 Hz
-			processSample(lowPassFilter((int)circBuf.getLast()));
-			processSample(lowPassFilter(0.0));
-			processSample(lowPassFilter(0.0));
+			processSample(interpolateFilter.filter((int)circBuf.getLast()));
+			processSample(interpolateFilter.filter(0.0));
+			processSample(interpolateFilter.filter(0.0));
 		}
 		
 	}	
 	
-	// A 2500 Hz low pass filter
-	private double lowPassFilter (double in)	{
-		int a;
-		for (a=0;a<NZEROS_LOW;a++)	{
-			xvLow[a]=xvLow[a+1];
-			yvLow[a]=yvLow[a+1];
-		}
-		xvLow[NZEROS_LOW]=in/GAIN_LOW;
-		yvLow[10] =   (xvLow[0] + xvLow[10]) + 10 * (xvLow[1] + xvLow[9]) + 45 * (xvLow[2] + xvLow[8])
-                + 120 * (xvLow[3] + xvLow[7]) + 210 * (xvLow[4] + xvLow[6]) + 252 * xvLow[5]
-                + ( -0.0137757219 * yvLow[0]) + (  0.1953296201 * yvLow[1])
-                + ( -1.2624940545 * yvLow[2]) + (  4.9039991908 * yvLow[3])
-                + (-12.6953486280 * yvLow[4]) + ( 22.9248466450 * yvLow[5])
-                + (-29.3030999830 * yvLow[6]) + ( 26.2473202970 * yvLow[7])
-                + (-15.8199532880 * yvLow[8]) + (  5.8207343496 * yvLow[9]);	
-		return yvLow[NZEROS_LOW];
-	}
-	
+
 	// Process a 24000 Hz sample
 	private void processSample (double in)	{
 		
-	 NCO nco=new NCO(1800.0,0.25,24000);
+		theApp.debugDump(Double.toString(in));
+	 
+	}
 	
-	 int a;
-	 for (a=0;a<60000;a++)	{
-		 double v=nco.getSample();
-		 theApp.debugDump(Double.toString(v));
-	 }
-	 
-	 a++;
-	 
+	// A class which is a 2500 Hz low pass filter (sample rate 24000 Hz)
+	private class lowPass2500Filter	{
+		
+		private final int NZEROS_LOW=10;
+		private final double GAIN_LOW=4.194017117e+05;
+		private double xvLow[]=new double[NZEROS_LOW+1];
+		private double yvLow[]=new double[NZEROS_LOW+1];
+		
+		public double filter (double in)	{
+			int a;
+			for (a=0;a<NZEROS_LOW;a++)	{
+				xvLow[a]=xvLow[a+1];
+				yvLow[a]=yvLow[a+1];
+			}
+			xvLow[NZEROS_LOW]=in/GAIN_LOW;
+			yvLow[10] =   (xvLow[0] + xvLow[10]) + 10 * (xvLow[1] + xvLow[9]) + 45 * (xvLow[2] + xvLow[8])
+	                + 120 * (xvLow[3] + xvLow[7]) + 210 * (xvLow[4] + xvLow[6]) + 252 * xvLow[5]
+	                + ( -0.0137757219 * yvLow[0]) + (  0.1953296201 * yvLow[1])
+	                + ( -1.2624940545 * yvLow[2]) + (  4.9039991908 * yvLow[3])
+	                + (-12.6953486280 * yvLow[4]) + ( 22.9248466450 * yvLow[5])
+	                + (-29.3030999830 * yvLow[6]) + ( 26.2473202970 * yvLow[7])
+	                + (-15.8199532880 * yvLow[8]) + (  5.8207343496 * yvLow[9]);	
+			return yvLow[NZEROS_LOW];
+		}
+		
 	}
 	
 	
